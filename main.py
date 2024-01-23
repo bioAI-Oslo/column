3b44f0e49b75955d470f98f6428e547741c05070
Mia-Katrin Ose Kvalsund <mia.kvalsund@gmail.com>
2024-01-23 18:58:13 +0100

    Updating main

--- main.py
+++ main.py
@@ -34,82 +34,29 @@ np.random.seed(0)
 random.seed(0)
 tf.random.set_seed(0)
 
-# TODO: Move code below to if __name__ == "__main__"
-
-# Parse arguments
-parser = argparse.ArgumentParser(
-    prog="Main", description="This program runs an optimization.", epilog="Text at the bottom of help"
-)
-parser.add_argument("-c", "--config", type=str, help="The config file to use", default="config")
-parser.add_argument("-sf", "--sub_folder", type=str, help="The sub folder to use", default=None)
-parser.add_argument(
-    "-cp",
-    "--continue_path",
-    type=str,
-    help="The path to continue the run of. If not specified, starts a new run",
-    default=None,
-)
-parser.add_argument("-v", "--visualize", action="store_true", help="Visualize mode")
-parser.add_argument("-s", "--save", action="store_true", help="Save data")
-parser.add_argument("-tp", "--test_path", type=str, help="If not specified, defaults to training", default=None)
-parser.add_argument("-vn", "--vis_num", type=int, help="Number of inferences to visualize", default=1)
-
-args = parser.parse_args()
-
-# Read config
-config.read("config")
-mnist_digits = eval(config.dataset.mnist_digits)
-loss_function = eval(config.training.loss)
-predicting_method = eval(config.training.predicting_method)
-
-# Constants
-N_DATAPOINTS = len(mnist_digits) * config.dataset.samples_per_digit
-CLASS_CHANNELS = len(mnist_digits)
-ACT_CHANNELS = 2 * int(config.network.moving)
-
-# Script wide functions
-data_func = get_MNIST_data
-kwargs = {
-    "MNIST_DIGITS": mnist_digits,
-    "SAMPLES_PER_DIGIT": config.dataset.samples_per_digit,
-    "verbose": False,
-}
-
-# Constants that will be used
-NUM_CHANNELS = config.network.img_channels + config.network.hidden_channels + CLASS_CHANNELS
-end_of_class = NUM_CHANNELS
-input_dim = NUM_CHANNELS
-output_dim = config.network.hidden_channels + CLASS_CHANNELS + ACT_CHANNELS
-
 
 # Evaluate one solution
 def evaluate_nca(
     flat_weights,
     training_data,
     target_data,
+    moving_nca_kwargs,
+    loss_function,
+    predicting_method,
     verbose=False,
     visualize=False,
     N_neo=None,
     M_neo=None,
     return_accuracy=False,
+    pool_training=False,
 ):
-    network = MovingNCA.get_instance_with(
-        flat_weights,
-        size_neo=(N_neo, M_neo),
-        size_image=training_data.shape[1:3],
-        num_classes=CLASS_CHANNELS,
-        num_hidden=config.network.hidden_channels,
-        iterations=config.network.iterations,
-        current_pos=config.network.current_pos,
-        moving=config.network.moving,
-        mnist_digits=mnist_digits,
-    )
+    network = MovingNCA.get_instance_with(flat_weights, size_neo=(N_neo, M_neo), **moving_nca_kwargs)
 
     loss = 0
     accuracy = 0
     visualized = 0
     for sample, (img_raw, expected) in enumerate(zip(training_data, target_data)):
-        if not config.training.pool_training or (config.training.pool_training and sample != 0 and sample % 2 == 0):
+        if not pool_training or (pool_training and sample != 0 and sample % 2 == 0):
             network.reset()
 
         # Code further on requires a 3D image. TODO See if this is necessary
@@ -133,52 +80,61 @@ def evaluate_nca(
     if verbose:
         print("Accuracy:", np.round(accuracy * 100 / training_data.shape[0], 2), "%")
 
+    scaled_loss = scale_loss(loss, training_data.shape[0])
     if return_accuracy:
-        return scale_loss(loss, N_DATAPOINTS), float(accuracy) / float(training_data.shape[0])
-    return scale_loss(loss, N_DATAPOINTS)
+        return scaled_loss, float(accuracy) / float(training_data.shape[0])
+    return scaled_loss
 
 
-def run_optimize():
+def run_optimize(
+    config,
+    moving_nca_kwargs,
+    loss_function,
+    predicting_method,
+    data_func,
+    data_kwargs,
+    continue_path=None,
+    save=False,
+    sub_folder=None,
+):
+    # Print amount of weights
     _, _, weight_amount = get_weights_info(
-        MovingNCA(
-            num_classes=CLASS_CHANNELS,
-            num_hidden=config.network.hidden_channels,
-            iterations=config.network.iterations,
-            current_pos=config.network.current_pos,
-            moving=config.network.moving,
-            size_neo=(config.scale.train_n_neo, config.scale.train_m_neo),
-            size_image=(28, 28),
-        ).weights
+        MovingNCA(size_neo=(config.scale.train_n_neo, config.scale.train_m_neo), **moving_nca_kwargs).weights
     )
     print("\nWeights:", int(weight_amount), "\n")
 
     # Init solution for the ES to initialize
     init_sol = None
-    if args.continue_path is not None:
-        init_sol = Logger.load_checkpoint(args.continue_path)
+    if continue_path is not None:
+        init_sol = Logger.load_checkpoint(continue_path)
     else:
         init_sol = int(weight_amount.numpy()) * [0.0]
+
     es = cma.CMAEvolutionStrategy(init_sol, 0.001)  # 0.001
+    np.random.seed(0)  # TODO: REMOVE THIS
 
     weight_amount = None
     init_sol = None
 
+    # Init logger
     logger_object = None
     generation_numbers = None
-    if args.continue_path is not None:
-        logger_object = Logger.continue_run(config, args.continue_path, save=args.save)
+    if continue_path is not None:
+        logger_object = Logger.continue_run(config, continue_path, save=save)
         start_point = logger_object.data["x_axis"][-1]
         generation_numbers = range(start_point + 1, config.training.maxgen + start_point + 1)
     else:
-        logger_object = Logger(config, save=args.save)
+        logger_object = Logger(config, save=save, sub_folder=sub_folder)
         generation_numbers = range(0, config.training.maxgen + 1)
 
+    # Init pool
     pool = None
     if config.training.threads > 1:
         print("We're starting pools with", config.training.threads, "threads")
         mp.set_start_method("spawn")
         pool = mp.Pool(config.training.threads)  # , maxtasksperchild=10)
 
+    # Record bestever solution
     bestever_score = np.inf
     bestever_weights = None
 
@@ -188,64 +144,64 @@ def run_optimize():
             start_time = time.time()
             print()
             print("Generation", g, flush=True)
-            solutions = es.ask(number=config.training.popsize)  # , sigma_fac=(((MAXGEN-g)/MAXGEN)*0.9)+0.1)
-            training_data, target_data = data_func(**kwargs)
 
+            # Get candidate solutions based on CMA-ES internal parameters
+            solutions = es.ask(number=config.training.popsize)  # , sigma_fac=(((MAXGEN-g)/MAXGEN)*0.9)+0.1)
+
+            # Generate training data for evaluating each candidate solution
+            training_data, target_data = data_func(**data_kwargs)
+
+            eval_kwargs = {
+                "training_data": training_data,
+                "target_data": target_data,
+                "moving_nca_kwargs": moving_nca_kwargs,
+                "loss_function": loss_function,
+                "predicting_method": predicting_method,
+                "verbose": False,
+                "visualize": False,
+                "N_neo": config.scale.train_n_neo,
+                "M_neo": config.scale.train_m_neo,
+                "return_accuracy": False,
+                "pool_training": False,
+            }
+
+            # Evaluate each candidate solution
             if pool is None:
-                solutions_fitness = [evaluate_nca(s, training_data, target_data) for s in solutions]
+                solutions_fitness = [evaluate_nca(s, **eval_kwargs) for s in solutions]
             else:
-                jobs = [
-                    pool.apply_async(
-                        evaluate_nca,
-                        args=(
-                            s,
-                            training_data,
-                            target_data,
-                            False,
-                            False,
-                            config.scale.train_n_neo,
-                            config.scale.train_m_neo,
-                        ),
-                    )
-                    for s in solutions
-                ]
+                jobs = [pool.apply_async(evaluate_nca, args=[s], kwds=eval_kwargs) for s in solutions]
                 solutions_fitness = [job.get() for job in jobs]
+
+            # Tell es what the result was. It uses this to update its parameters
             es.tell(solutions, solutions_fitness)
 
+            # Record winner for plotting and the future
             if np.min(solutions_fitness) < bestever_score:
                 bestever_score = np.min(solutions_fitness)
                 bestever_weights = solutions[np.argmin(solutions_fitness)]
 
-            print("Best score:", np.min(solutions_fitness), flush=True)
-            print("Bestever score:", bestever_score, flush=True)
-
+            # Plotting and visualization starts here
             visualize_this_gen = args.visualize and g % config.logging.visualize_interval == 0
             if g % config.logging.plotting_interval == 0 or visualize_this_gen:
                 winner_flat = solutions[np.argmin(solutions_fitness)]
 
-                testing_data, target_data_test = data_func(**kwargs)
+                testing_data, target_data_test = data_func(**data_kwargs)
 
-                # TODO: args dictionary to avoid clutter
-                loss_train_size, acc_train_size = evaluate_nca(
-                    winner_flat,
-                    testing_data,
-                    target_data_test,
-                    verbose=visualize_this_gen,
-                    visualize=visualize_this_gen,
-                    return_accuracy=True,
-                    N_neo=config.scale.train_n_neo,
-                    M_neo=config.scale.train_m_neo,
-                )
-                loss_test_size, acc_test_size = evaluate_nca(
-                    winner_flat,
-                    testing_data,
-                    target_data_test,
-                    verbose=visualize_this_gen,
-                    visualize=visualize_this_gen,
-                    return_accuracy=True,
-                    N_neo=config.scale.test_n_neo,
-                    M_neo=config.scale.test_m_neo,
-                )
+                # Alter kwargs for testing. We don't need to change it back
+                eval_kwargs["training_data"] = testing_data
+                eval_kwargs["target_data"] = target_data_test
+                eval_kwargs["verbose"] = visualize_this_gen
+                eval_kwargs["visualize"] = visualize_this_gen
+                eval_kwargs["return_accuracy"] = True
+
+                # We don't have to change the neo size because it's already train size
+                loss_train_size, acc_train_size = evaluate_nca(winner_flat, **eval_kwargs)
+
+                # Alter specific neo sizes for testing size
+                eval_kwargs["N_neo"] = config.scale.test_n_neo
+                eval_kwargs["M_neo"] = config.scale.test_m_neo
+
+                loss_test_size, acc_test_size = evaluate_nca(winner_flat, **eval_kwargs)
 
                 testing_data, target_data_test = None, None
 
@@ -258,36 +214,100 @@ def run_optimize():
                         loss_test_size,
                         bestever_score,
                     )
+            # Plotting and visualization ends here
 
-            if args.save and g % config.logging.saving_interval == 0:
+            # Do we need to save a little bit of data?
+            if save and g % config.logging.saving_interval == 0:
                 current_best_weights = solutions[np.argmin(solutions_fitness)]
                 logger_object.save_checkpoint(current_best_weights, filename="best_network")
                 logger_object.save_checkpoint(bestever_weights, filename="bestever_network")
                 logger_object.save_plotting_data()
 
+            # Display results
+            print("Current best score:", np.min(solutions_fitness), flush=True)
+            print("Bestever score:", bestever_score, flush=True)
             end_time = time.time()
             print("Generation time:", end_time - start_time, "seconds", flush=True)
+
         end_run_time = time.time()
         print("The entire run took", end_run_time - start_run_time, "seconds")
 
     except KeyboardInterrupt:
         input("You've cancelled the run. Press enter.")
 
+    # Close pool if it exists
     if pool is not None:
         pool.close()
 
-    if args.save:
+    # Do we need to save a little bit of data?
+    if save:
         current_best_weights = solutions[np.argmin(solutions_fitness)]
         logger_object.save_checkpoint(current_best_weights, filename="best_network")
         logger_object.save_checkpoint(bestever_weights, filename="bestever_network")
         logger_object.save_plotting_data()
 
+    # Winner is best solution. But it could be best individual of last generation
+    # Luckily both are saved for future use
     return bestever_weights
 
 
 if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        prog="Main", description="This program runs an optimization.", epilog="Text at the bottom of help"
+    )
+    parser.add_argument("-c", "--config", type=str, help="The config file to use", default="config")
+    parser.add_argument("-sf", "--sub_folder", type=str, help="The sub folder to use", default=None)
+    parser.add_argument(
+        "-cp",
+        "--continue_path",
+        type=str,
+        help="The path to continue the run of. If not specified, starts a new run",
+        default=None,
+    )
+    parser.add_argument("-v", "--visualize", action="store_true", help="Visualize mode")
+    parser.add_argument("-s", "--save", action="store_true", help="Save data")
+    parser.add_argument("-tp", "--test_path", type=str, help="If not specified, defaults to training", default=None)
+    parser.add_argument("-vn", "--vis_num", type=int, help="Number of inferences to visualize", default=1)
+
+    args = parser.parse_args()
+
+    # Read config
+    config.read(args.config)
+    mnist_digits = eval(config.dataset.mnist_digits)
+    loss_function = eval(config.training.loss)
+    predicting_method = eval(config.training.predicting_method)
+
+    moving_nca_kwargs = {
+        "size_image": (28, 28),
+        "num_classes": len(mnist_digits),
+        "num_hidden": config.network.hidden_channels,
+        "iterations": config.network.iterations,
+        "current_pos": config.network.current_pos,
+        "moving": config.network.moving,
+        "mnist_digits": mnist_digits,
+    }
+
+    # Script wide functions
+    data_func = get_MNIST_data
+    kwargs = {
+        "MNIST_DIGITS": mnist_digits,
+        "SAMPLES_PER_DIGIT": config.dataset.samples_per_digit,
+        "verbose": False,
+    }
+
     if args.test_path is None:
-        winner_flat = run_optimize()
+        winner_flat = run_optimize(
+            config=config,
+            moving_nca_kwargs=moving_nca_kwargs,
+            loss_function=loss_function,
+            predicting_method=predicting_method,
+            data_func=data_func,
+            data_kwargs=kwargs,
+            continue_path=args.continue_path,
+            save=args.save,
+            sub_folder=args.sub_folder,
+        )
     else:
         winner_flat = Logger.load_checkpoint(args.test_path)
 
@@ -298,6 +318,9 @@ if __name__ == "__main__":
         winner_flat,
         training_data,
         target_data,
+        moving_nca_kwargs,
+        loss_function,
+        predicting_method,
         verbose=True,
         visualize=args.visualize,
         return_accuracy=True,
