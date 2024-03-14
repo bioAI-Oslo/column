bafedee09d8924c7b344477c06756d9208c133ec
Mia-Katrin Ose Kvalsund <mia.kvalsund@gmail.com>
2024-03-14 17:12:34 +0100

    Oops added relative import of utils

--- src/cifar_processing.py
+++ src/cifar_processing.py
@@ -5,7 +5,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from keras.datasets import cifar10
-from utils import shuffle
+from src.utils import shuffle
 
 sorted_X_train = None
 sorted_X_test = None
