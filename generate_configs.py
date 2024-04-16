8ae6ad2502f7a0784e15e462b47750d22c44e52e
Mia-Katrin Ose Kvalsund <mia.kvalsund@gmail.com>
2024-04-16 11:14:04 +0200

    Fixing it so that configs get names

--- generate_configs.py
+++ generate_configs.py
@@ -5,6 +5,7 @@ from localconfig import config
 parser = ArgumentParser()
 parser.add_argument("-hc", "--hidden_channels", nargs="+", help="Number of hidden channels")
 parser.add_argument("-hn", "--hidden_neurons", nargs="+", help="Number of hidden neurons")
+parser.add_argument("-n", "--name_addon", type=str, help="The name to add on config", default="")
 args = parser.parse_args()
 
 config.read("config")
@@ -13,4 +14,4 @@ for hc in args.hidden_channels:
     for hn in args.hidden_neurons:
         config.network.hidden_channels = hc
         config.network.hidden_neurons = hn
-        config.save("config" + str(hc) + "_" + str(hn))
+        config.save("config" + args.name_addon + str(hc) + "_" + str(hn))
