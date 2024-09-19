1836a42d98d2580bc396892716a543595df360ec
Mia-Katrin Ose Kvalsund <mia.kvalsund@gmail.com>
2024-09-19 15:55:38 +0200

    Now using test set, with more

--- zero_shot_scalability.py
+++ zero_shot_scalability.py
@@ -1,7 +1,9 @@
+import json
 import os
 
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 from localconfig import config
 from main import evaluate_nca, evaluate_nca_batch, get_from_config
 from src.data_processing import get_labels, get_MNIST_data, get_MNIST_fashion_data
@@ -36,6 +38,7 @@ def get_score_subfolder(sub_path):
     # Get the data to use for all the tests on this network
     print("Fetching data")
     kwargs["SAMPLES_PER_CLASS"] = NUM_DATA
+    kwargs["test"] = True
     # Get test data for new evaluation
     test_data, target_data = data_func(**kwargs)
 
@@ -66,6 +69,7 @@ def plot_all_same_train_neo(to_test, NUM_DATA, path):
     # Record all accuracy and loss lines, means and stds are taken afterwards
     mean_acc = []
     mean_loss = []
+    paths = []
 
     # Loop start
     for numebr, sub_folder in enumerate(os.listdir(path)):  # For each subfolder in folder path
@@ -75,6 +79,7 @@ def plot_all_same_train_neo(to_test, NUM_DATA, path):
 
             mean_acc.append(result_acc)
             mean_loss.append(result_loss)
+            paths.append(sub_path)
 
     ###### PLOT THE FINDINGS
 
@@ -86,13 +91,13 @@ def plot_all_same_train_neo(to_test, NUM_DATA, path):
     ax.plot(to_test, [0.2 for _ in to_test], label="Random accuracy", color="black")
 
     # Plot standard deviation for loss and accuracy
-    ax.fill_between(
+    """ax.fill_between(
         to_test,
         np.mean(mean_loss, axis=0) - np.std(mean_loss, axis=0),
         np.mean(mean_loss, axis=0) + np.std(mean_loss, axis=0),
         color=cmap(0.2),
         alpha=0.5,
-    )
+    )"""
     ax.fill_between(
         to_test,
         np.mean(mean_acc, axis=0) - np.std(mean_acc, axis=0),
@@ -102,7 +107,7 @@ def plot_all_same_train_neo(to_test, NUM_DATA, path):
     )
 
     # Plot mean loss and accuracy
-    ax.plot(to_test, np.mean(mean_loss, axis=0), label="Loss", color=cmap(0.2))
+    # ax.plot(to_test, np.mean(mean_loss, axis=0), label="Loss", color=cmap(0.2))
     ax.plot(to_test, np.mean(mean_acc, axis=0), label="Accuracy", color=cmap(0.8))
 
     # Plot best network's accuracy
@@ -129,11 +134,11 @@ def plot_all_same_train_neo(to_test, NUM_DATA, path):
     ################################ Absolute figure ##################################
 
     # Compare all the networks to their trained for size
-    size_trained = np.argmin(np.abs(to_test - config.scale.train_n_neo))
+    size_trained = np.argmin(np.abs(to_test - train_n_neo))
 
     mean_acc_compared = []
     for scale_score in mean_acc:
-        mean_acc_compared.append(scale_score[size_trained] - np.array(scale_score))
+        mean_acc_compared.append((np.array(scale_score) - 0.2) / (scale_score[size_trained] - 0.2))
 
     fig, ax = plt.subplots(1)
 
@@ -141,7 +146,7 @@ def plot_all_same_train_neo(to_test, NUM_DATA, path):
     for i, scale_score in enumerate(mean_acc_compared):
         ax.plot(
             to_test,
-            1 - scale_score,
+            scale_score,
             color=cmap(i / (len(mean_acc_compared) - 1) if len(mean_acc_compared) > 1 else 0.5),
         )
 
@@ -152,18 +157,40 @@ def plot_all_same_train_neo(to_test, NUM_DATA, path):
     ax.set_xlabel("NCA size (^2)")
     ax.set_ylabel("Retained accuracy (%)")
 
+    ################################ All lines figure ##################################
+
+    fig, ax = plt.subplots(1)
+
+    for i, scale_score in enumerate(mean_acc):
+        plt.plot(
+            to_test, scale_score, color=cmap(i / (len(mean_acc) - 1) if len(mean_acc) > 1 else 0.5), label=paths[i]
+        )
+
+    ax.legend()
+
+    # Make sure axis goes down to 0
+    ax.set_ylim(ymin=0)
+    ax.set_xticks(to_test, to_test)
+    ax.set_yticks(np.arange(0, 1.1, 0.1), range(0, 110, 10))
+    ax.set_xlabel("NCA size (^2)")
+    ax.set_ylabel("Accuracy (%)")
+
     plt.show()
 
 
-def plot_different_train_neos(to_test, NUM_DATA, path):
+def plot_different_train_neos(to_test, NUM_DATA, path, save=False):
     # Record all accuracy and loss lines, means and stds are taken afterwards
     findings = {}
+
+    to_save = {}
 
     # Loop start
     for numebr, sub_folder in enumerate(os.listdir(path)):  # For each subfolder in folder path
         sub_path = path + "/" + sub_folder
         if os.path.isdir(sub_path):  # If it is a folder
             result_acc, result_loss, train_n_neo = get_score_subfolder(sub_path)
+
+            to_save[sub_path] = result_acc
 
             if str(train_n_neo) not in findings:
                 findings[str(train_n_neo)] = {}
@@ -172,6 +199,12 @@ def plot_different_train_neos(to_test, NUM_DATA, path):
             else:
                 findings[str(train_n_neo)]["acc"].append(result_acc)
                 findings[str(train_n_neo)]["loss"].append(result_loss)
+
+    # Save findings
+    if save:
+        to_save["test_sizes"] = to_test.tolist()
+        with open(path + "/scalabilities.json", "w") as outfile:
+            json.dump(to_save, outfile)
 
     # Plot findings
     cmap = plt.cm.plasma
@@ -211,7 +244,8 @@ def plot_different_train_neos(to_test, NUM_DATA, path):
 
 # Changeables
 to_test = np.arange(1, 26 + 1, 1)
-NUM_DATA = 10
-path = "experiments/neo_size_experiment5"
+# to_test = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26])
+NUM_DATA = 40
+path = "experiments/neo_size_experiment_nonmoving"
 
-plot_different_train_neos(to_test, NUM_DATA, path)
+plot_different_train_neos(to_test, NUM_DATA, path, save=True)
