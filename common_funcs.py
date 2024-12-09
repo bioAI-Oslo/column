02e707e550306bec02c966bea518ea15c859589b
Mia-Katrin Ose Kvalsund <mia.kvalsund@gmail.com>
2024-12-09 13:48:04 +0100

    Cleaned and added comments

--- common_funcs.py
+++ common_funcs.py
@@ -1,4 +1,7 @@
+""" Common functions for the column experiments """
+
 from main import get_from_config
+from src.active_nca import ActiveNCA
 from src.data_processing import (
     get_CIFAR_data,
     get_labels,
@@ -18,11 +21,25 @@ from src.loss import (
     pixel_wise_L2,
     pixel_wise_L2_and_CE,
 )
-from src.moving_nca_no_tf import MovingNCA
 from src.utils import get_config
 
 
 def get_network(sub_path, num_data, size_img=None):
+    """
+    Load a network and relevant data from a subexperiment.
+
+    Args:
+        sub_path (str): Subexperiment path.
+        num_data (int): Number of datapoints per class to load.
+        size_img (tuple of two ints, optional): Size of the images to use. If not provided, the size from the config will be used.
+
+    Returns:
+        ActiveNCA: The loaded network.
+        list of ints: The labels of the loaded data.
+        function: The function to use to load the data.
+        dict: The keyword arguments to pass to the data function.
+        function: The method to use for predicting the classes.
+    """
     winner_flat = Logger.load_checkpoint(sub_path)
 
     # Also load its config
@@ -41,7 +58,7 @@ def get_network(sub_path, num_data, size_img=None):
     labels = moving_nca_kwargs["labels"]
 
     # Load network
-    network = MovingNCA.get_instance_with(
+    network = ActiveNCA.get_instance_with(
         winner_flat, size_neo=(config.scale.train_n_neo, config.scale.train_m_neo), **moving_nca_kwargs
     )
 
