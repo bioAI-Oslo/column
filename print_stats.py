73d013fa7840f8e4eae7ac78332950d641fbec04
Mia-Katrin Ose Kvalsund <mia.kvalsund@gmail.com>
2024-12-09 16:05:21 +0100

    Cleaned and commented

--- print_stats.py
+++ print_stats.py
@@ -29,10 +29,24 @@ from src.utils import get_config
 
 
 def get_performance(sub_path, config, test_data_used=False, num_data=None):
+    """
+    Evaluate the performance of a trained network on a dataset.
+
+    Args:
+        sub_path (str): Path to the experiment subdirectory containing the network checkpoint.
+        config (Config): Configuration object containing experiment settings.
+        test_data_used (bool, optional): Whether to use test data for evaluation. Defaults to False.
+        num_data (int, optional): Number of data samples per class to use. If None, the maximum number
+                                  of balanced samples is used.
+
+    Returns:
+        float: The loss of the evaluated network.
+        float: The accuracy of the evaluated network.
+    """
     winner_flat = Logger.load_checkpoint(sub_path)
 
     # Fetch info from config and enable environment for testing
-    moving_nca_kwargs, loss_function, predicting_method, data_func, kwargs = get_from_config(config)
+    moving_nca_kwargs, _, predicting_method, data_func, kwargs = get_from_config(config)
 
     kwargs["test"] = test_data_used
 
@@ -48,7 +62,7 @@ def get_performance(sub_path, config, test_data_used=False, num_data=None):
         training_data,
         target_data,
         moving_nca_kwargs,
-        loss_function,
+        pixel_wise_CE,  # And not energy!!!
         predicting_method,
         verbose=False,
         visualize=False,
@@ -66,31 +80,42 @@ def get_performance(sub_path, config, test_data_used=False, num_data=None):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("path", type=str, help="The path to the experiment folder")
-    parser.add_argument("hc", type=int, help="The number of hidden channels")
-    parser.add_argument("hn", type=int, help="The number of hidden neurons")
+    parser.add_argument("-hc", type=int, default=None, help="The number of hidden channels")
+    parser.add_argument("-hn", type=int, default=None, help="The number of hidden neurons")
     args = parser.parse_args()
     path = args.path
 
-    num_data_train = None
+    num_data_train = None  # None basically means as much as possible
     num_data_test = None
 
+    # Get performance, train and test
     performances_train = []
     performances_test = []
 
+    # Test all?
+    test_all = args.hc is None and args.hn is None
+
+    # Get performance for all subfolders
     for sub_folder in os.listdir(path):
         sub_path = path + "/" + sub_folder
         if os.path.isdir(sub_path):
+
             config = get_config(sub_path)
             print(config.network.hidden_channels, config.network.hidden_neurons, args.hc, args.hn)
-            if config.network.hidden_channels != args.hc or config.network.hidden_neurons != args.hn:
+
+            # Skip if we are not testing all, and if the config doesn't match the requirements
+            if not test_all and (config.network.hidden_channels != args.hc or config.network.hidden_neurons != args.hn):
                 continue
 
+            # Get performance on train data
             loss, acc = get_performance(sub_path, config, test_data_used=False, num_data=num_data_train)
             performances_train.append((loss, acc, sub_path))
 
+            # Get performance on test data
             loss, acc = get_performance(sub_path, config, test_data_used=True, num_data=num_data_test)
             performances_test.append((loss, acc, sub_path))
 
+    # Get best performance, based on loss (min CE loss)
     best_performance = min(performances_train, key=lambda x: x[0])
 
     print()
@@ -106,6 +131,7 @@ if __name__ == "__main__":
     )
     print("Mean loss:", np.mean([x[0] for x in performances_train]), "std:", np.std([x[0] for x in performances_train]))
 
+    # Get test performance of the best individual on the test data
     best_performance_test = None
     for x in performances_test:
         if x[2] == best_performance[2]:
