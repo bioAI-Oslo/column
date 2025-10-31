04eee5f29e0ac69c31428dced35afb60804ff4b2
Mia-Katrin Ose Kvalsund <mia.kvalsund@gmail.com>
2025-10-31 18:19:01 +0100

    Adding docstrings and colored right answer

--- src/animate.py
+++ src/animate.py
@@ -2,31 +2,62 @@
 
 import gc
 
+import matplotlib
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-# From here:
-# https://stackoverflow.com/questions/56654952/how-to-mark-cells-in-matplotlib-pyplot-imshow-drawing-cell-borders
-# ImportanceOfBeingErnest: https://stackoverflow.com/users/4124317/importanceofbeingernest
 def highlight_cell(x, y, ax=None, last_rect=None, **kwargs):
-    # Origin of plt.Rectangle is upper left corner, just like plt.imshow
-    # I tested and found
-    # Rectangle's (0,20) is down from origin, (20,0) is right of origin
-    # plt.imshow's (0,20) is right and (20,0) is down
-    # Therefore, I switch x and y for Rectangle
-    # rect = plt.Rectangle((y-.5, x-.5), 1,1, fill=False, **kwargs)
+    """
+    Highlight the cell at x, y on the axis ax with a rectangle.
+
+    From here:
+    https://stackoverflow.com/questions/56654952/how-to-mark-cells-in-matplotlib-pyplot-imshow-drawing-cell-borders
+    ImportanceOfBeingErnest: https://stackoverflow.com/users/4124317/importanceofbeingernest
+
+    Origin of plt.Rectangle is upper left corner, just like plt.imshow
+    I tested and found
+    Rectangle's (0,20) is down from origin, (20,0) is right of origin
+    plt.imshow's (0,20) is right and (20,0) is down
+    Therefore, I switch x and y for Rectangle
+    rect = plt.Rectangle((y-.5, x-.5), 1,1, fill=False, **kwargs)
+
+    Args:
+        x, y (int): The coordinates of the cell to highlight.
+        ax (matplotlib.axes.Axes, optional): The axis on which to draw the rectangle. If None, use the current axis.
+        last_rect (matplotlib.patches.Rectangle, optional): The previous rectangle to remove. If None, do nothing.
+        **kwargs: Additional keyword arguments to pass to matplotlib.patches.Rectangle.
+
+    Returns:
+        matplotlib.patches.Rectangle: The created rectangle.
+    """
     rect = plt.Rectangle((y - 1.5, x - 1.5), 3, 3, fill=False, alpha=0.4, **kwargs)
     ax = ax or plt.gca()
     ax.add_patch(rect)
-    if last_rect is not None:
+    if last_rect is not None:  # Garbage collection was necessary because of memory leak
         last_rect.remove()
         del last_rect
     return rect
 
 
-def animate(images, states, actions, perceptions_through_time, hidden_channels, class_channels, labels):
+def animate(
+    images, states, actions, perceptions_through_time, hidden_channels, class_channels, labels, correct_label_index
+):
+    """
+    Animate the classification process of the Active NCA model.
+
+    Parameters:
+        images (list of np.ndarrays): The images to classify.
+        states (list of np.ndarrays): The states of the model at each step.
+        actions (list of np.ndarrays or None): The actions taken by the model at each step. If None, the actions are not plotted.
+        perceptions_through_time (list of np.ndarrays): The perceptions of the model at each step.
+        hidden_channels (int): The number of hidden channels of the model.
+        class_channels (int): The number of class channels of the model.
+        labels (list of str): The labels of the classes.
+        correct_label_index (int): The index of the correct label.
+    """
+    # Set up the figure, sized according to the number of classes
     if class_channels == 10:
         fig = plt.figure(figsize=(15, 5))
         plt.subplots_adjust(top=0.8, bottom=0, hspace=0)
@@ -52,6 +83,12 @@ def animate(images, states, actions, perceptions_through_time, hidden_channels, 
     # Get amount of images in the horizontal direction
     max_images_on_line = max(hidden_channels + 1 + (2 * (actions is not None)), class_channels)
 
+    # IBM design library palette
+    """cmap = matplotlib.colors.LinearSegmentedColormap.from_list(
+        "", ["#f17720", "#ffa630", "#ffffff", "#00a7e1", "#0474ba"]
+    )"""
+    cmap = "RdBu"
+
     # Plot image
     ax_img = fig.add_subplot(2, max_images_on_line, 1)
     if len(images[0][0][0]) == 1:
@@ -66,7 +103,7 @@ def animate(images, states, actions, perceptions_through_time, hidden_channels, 
     im_hidden_list = []
     for j in range(hidden_channels):
         ax_hidden = fig.add_subplot(2, max_images_on_line, 2 + j)
-        im_hidden = ax_hidden.imshow(states[0, :, :, j], cmap="RdBu", vmin=-1, vmax=1)
+        im_hidden = ax_hidden.imshow(states[0, :, :, j], cmap=cmap, vmin=-1, vmax=1)
         im_hidden_list.append(im_hidden)
         ax_hidden.set_title("Hidden\nchannel " + str(j + 1))
         ax_hidden.set_yticks([])
@@ -90,7 +127,7 @@ def animate(images, states, actions, perceptions_through_time, hidden_channels, 
     ax_class_list = []
     for j in range(class_channels):
         ax_class = fig.add_subplot(2, max_images_on_line, 1 + max_images_on_line + j)
-        im_class = ax_class.imshow(states[0, :, :, hidden_channels + j], cmap="RdBu", vmin=-1, vmax=1)
+        im_class = ax_class.imshow(states[0, :, :, hidden_channels + j], cmap=cmap, vmin=-1, vmax=1)
         im_class_list.append(im_class)
         ax_class_list.append(ax_class)
         ax_class.set_title("Class\n" + str(labels[j]))
@@ -107,7 +144,10 @@ def animate(images, states, actions, perceptions_through_time, hidden_channels, 
         state = states[i]
         believed = np.argmax(np.mean(state[1:-1, 1:-1, -class_channels:], axis=(0, 1)))
         super_ax.set_title(
-            f"Timestep: {i}\nCurrent classification: " + str(labels[believed]),
+            f"Timestep: {i}\nCurrent classification: "
+            + str(labels[believed])
+            + "\nGround truth: "
+            + str(labels[correct_label_index]),
             pad=20,
             ha="left",
             x=0,
@@ -126,7 +166,10 @@ def animate(images, states, actions, perceptions_through_time, hidden_channels, 
         for j in range(class_channels):
             im_class_list[j].set_array(states[i, :, :, hidden_channels + j])
             if believed == j:
-                ax_class_list[j].set_title("Class\n" + str(labels[j]), color="deepskyblue", weight="bold")
+                if j == correct_label_index:
+                    ax_class_list[j].set_title("Class\n" + str(labels[j]), color="#30C14E", weight="bold")
+                else:
+                    ax_class_list[j].set_title("Class\n" + str(labels[j]), color="#DC2381", weight="bold")
             else:
                 ax_class_list[j].set_title("Class\n" + str(labels[j]), color="black")
 
